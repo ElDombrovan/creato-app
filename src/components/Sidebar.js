2d5030093c7d65cad8007959e66eaae16d9280f4
El Dombrovan <el.dombrovan@gmail.com>
2020-02-12 13:43:21 +0200

    component(style): add subgrid for Sidebar component

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -6,11 +6,11 @@ import { IconsPanel } from "./IconsPanel.js";
 export const Sidebar = () => {
     return(
         <div className="Sidebar">
-            <div className="Sidebar__image--icon">
+            <div className="Sidebar__image--menu">
             <Icon name="menu"/>
             </div>
             <div className="Sidebar__image--logo">
-                <img src="../../public/icon_logo.png" alt="logo" />
+                <img src="../public/icon_logo.png" alt="logo" />
             </div>
             <IconsPanel />
         </div>
